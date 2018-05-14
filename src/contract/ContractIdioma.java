ff1845b2bdc75adb835849c60c70c8fea79e6d04
Albert Corominas Mairscot <albertcorominasmariscot@gmail.com>
2018-05-14 18:55:13 +0200

    Canvis Contracte Idioma

--- src/contract/ContractIdioma.java
+++ src/contract/ContractIdioma.java
@@ -1,5 +1,8 @@
 package contract;
 
+import java.sql.Types;
+import java.util.HashMap;
+
 /**
  * @author albertCorominas
  */
@@ -7,4 +10,8 @@ public abstract class ContractIdioma {
     public static final String NOM_TAULA = "idioma";
     public static final String ID = "id";
     public static final String NOM = "nom";
+    public static final HashMap<String,Integer> DEFINICIO = new HashMap<String,Integer>(){{
+        put(ID, Types.INTEGER);
+        put(NOM, Types.VARCHAR);
+    }};
 }
